268ca369afc37a058098de4a4f193d3b1a775608
rdthrdn <137570361+rdthrdn@users.noreply.github.com>
2024-03-25 21:55:48 +0700

    Update Sandbox.sh

--- soal_1/Sandbox.sh
+++ soal_1/Sandbox.sh
@@ -4,26 +4,16 @@
 
 # subsoal a
 echo "a. pembeli dengan total sales paling tinggi: "
-tail -n +2 Sandbox.csv | awk 'BEGIN { max=0 } $17 > max { max=$17; name=$6 } END { print name }' FS=","
-max=$(tail -n +2 Sandbox.csv | awk 'BEGIN { max=0 } $17 > max { max=$17 } END { print max }' FS=",")
+tail -n +2 Sandbox.csv | awk 'BEGIN { max=0 } $17 > max { max=$17; name=$6 } END { print name " " max }' FS=","
 
 # subsoal b
 echo "b. customer segment yang memiliki profit paling kecil: "
-tail -n +2 Sandbox.csv | awk -v min="$max" 'BEGIN { FS="," } $20 < min { min=$20; segment=$7 } END { print segment }'
+max=$(tail -n +2 Sandbox.csv | awk 'BEGIN { max=0 } $20 > max { max=$20 } END { print max }' FS=",")
+tail -n +2 Sandbox.csv | awk -v min=$max 'BEGIN { } $20 < min { min=$20; segment=$7 } END { print segment " " min }' FS=","
 
 # subsoal c
-echo "c. 1. kategori dengan profit tertinggi: "
-cat=$(tail -n +2 Sandbox.csv | awk 'BEGIN { maxxwyn=0 } $20 > maxwynn { maxwynn=$20; cat=$14 } END { print cat }' FS=",")
-maxwynn=$(tail -n +2 Sandbox.csv | awk 'BEGIN { maxxwyn=0 } $20 > maxwynn { maxwynn=$20 } END { print maxwynn }' FS=",")
-echo $cat
-
-echo "c. 2. kategori dengan profit tertinggi kedua: "
-catwo=$(tail -n +2 Sandbox.csv | awk -v maxwynn="$maxwynn" -v cat="$cat" 'BEGIN { maxtwo=0; FS="," } $20 > maxtwo && $20 < maxwynn && $14 != cat { maxtwo=$20; catwo=$14 } END { print catwo }')
-maxtwo=$(tail -n +2 Sandbox.csv | awk -v maxwynn="$maxwynn" 'BEGIN { maxtwo=0; FS="," } $20 > maxtwo && $20 < maxwynn { maxtwo=$20 } END { print maxtwo }')
-echo $catwo
-
-echo "c. 3. kategori dengan profit tertinggi ketiga: "
-tail -n +2 Sandbox.csv | awk -v maxtwo="$maxtwo" -v cat="$cat" -v catwo="$catwo" 'BEGIN { maxthree=0; FS="," } $20 > maxthree && $20 < maxtwo && $14 != cat && $14 != catwo { maxthree=$20; cathree=$14 } END { print cathree }'
+echo "c. 3 total profit tertinggi berdasarkan kategori: "
+tail -n +2 Sandbox.csv | awk -F ',' '{sum[$14]+=$20} END {for (category in sum) print category " " sum[category]}' | sort -k2 -nr | head -n 3
 
 # subsoal d
 echo "d. pesanan atas nama adriaens: "
@@ -33,3 +23,5 @@ grep -i "Adriaens .*" Sandbox.csv | awk -F ',' '{print "quantity: " $18}'
 # beberapa sumber yang digunakan
 # https://stackoverflow.com/questions/28790371/bash-finding-maximum-value-in-a-particular-csv-column
 # https://stackoverflow.com/questions/19075671/how-do-i-use-shell-variables-in-an-awk-script
+# https://unix.stackexchange.com/questions/242946/using-awk-to-sum-the-values-of-a-column-based-on-the-values-of-another-column
+# https://linuxhandbook.com/sort-command/
